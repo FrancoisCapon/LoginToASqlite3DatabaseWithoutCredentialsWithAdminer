6d7dff4140c356f81a177ec8230688eeec095a8f
francois-capon <46624375+francois-capon@users.noreply.github.com>
2019-06-08 12:53:24 +0200

    Create build-adminer-4-sqlite3-into-one-file.sh

---
+++ build-adminer-4-sqlite3-into-one-file.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# author : François Capon
+
+# define the URLs
+ADMINER=https://www.adminer.org/latest.php
+PLUGIN=https://raw.github.com/vrana/adminer/master/plugins/plugin.php
+MYPLUGIN=https://github.com/FrancoisCapon/LoginToASqlite3DatabaseWithoutCredentialsWithAdminer/raw/master/fc-sqlite-connection-without-credentials.php
+# load and concatenate into one file
+wget -O adminer-4-sqilte3.php $ADMINER $PLUGIN $MYPLUGIN
+
+# add myplugin to Adminer
+# http://tldp.org/LDP/abs/html/here-docs.html#EX71C
+cat << "EOPHP" >> adminer-4-sqilte3.php
+
+function adminer_object() {
+    $plugins = array(new FCSqliteConnectionWithoutCredentials());
+    return new AdminerPlugin($plugins);
+}
+EOPHP
+# remove all the <?php except the first
+# http://www.theunixschool.com/2011/02/sed-replace-or-substitute-file-contents.html 
+sed -i '2,${s/<?php$//;}' adminer-4-sqilte3.php
