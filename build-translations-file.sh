5b20c00bf3c010bb07f67bfd2a71804f405f59d8
FrancoisCapon <github@francoiscapon.com>
2020-11-11 13:29:45 +0100

    Add the build translations file script

---
+++ build-translations-file.sh
@@ -0,0 +1,13 @@
+translationsFile="warning.translations.csv"
+rm $translationsFile
+touch $translationsFile
+for file in adminer/adminer/lang/*
+do
+    if [ -f $file ]; then
+        language="$(echo $(basename "$file") | cut -d'.' -f 1)"
+        if [ "$language" != "xx" ] && [ "$language" != "en" ]
+        then
+            echo $language',' >> $translationsFile
+        fi
+    fi
+done
